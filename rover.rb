70dc235d7f554d3a3968b1a3ebefe7d0f79a8702
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-14 14:05:06 -0500

    rover.rb file created and class Rover created.

---
+++ rover.rb
@@ -0,0 +1,22 @@
+class Rover
+
+
+  def initialize(x, y, dir)
+    @x = x
+    @y = y
+    @dir = dir
+  end
+
+  def read_instruction()
+    
+  end
+
+  def move
+
+  end
+
+  def turn
+
+  end
+
+end
