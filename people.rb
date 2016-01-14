e15b30e51a2dcd5c850f4a616bed8c7291b07ba4
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-14 13:33:23 -0500

    people.rb runs well until it breaks because we broke it on purpose; we called undefined methods on classes that don't share each others' methods.

--- people.rb
+++ people.rb
@@ -31,4 +31,15 @@ chris = Instructor.new("Chris")
 chris.greet
 
 student1 = Student.new("Cristina")
-student1.greet 
+student1.greet
+
+chris.teach
+student1.learn
+
+puts "Now I'm going to call #teach on Cristina the student and #teach on Chris the teacher. It won't work because those methods aren't shared between their classes; it would work if those methods were in the 'Person' class."
+
+#Program crashes NOW:
+student1.teach
+chris.learn
+
+#Specifically, because the #teach method is undefined or Student class and the #learn method is undefined for Instructor Class.
