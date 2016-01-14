57ed4a17c974c66ec579f9b9854fe2965e52aac3
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-14 13:27:05 -0500

    people.rb Set all classes and methods and now about to test instances.

--- people.rb
+++ people.rb
@@ -1,9 +1,34 @@
-class Student
+class Person
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hi, my name is #{@name}"
+  end
 
 end
 
-class Instructor
-
+class Student < Person
+  def learn
+    puts "I get it!"
+  end
 
 end
+
+class Instructor < Person
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+
+end
+
+# end of classes and methods and now to call them:
+
+chris = Instructor.new("Chris")
+chris.greet
+
+student1 = Student.new("Cristina")
+student1.greet 
