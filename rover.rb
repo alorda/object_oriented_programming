d3597c30b885438b1054c9c5196fb6b3dc8e484f
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-14 17:16:34 -0500

    rover.rb first method initialize works in Rover class; gets plateau size, coordinates, and direction of rover.

--- rover.rb
+++ rover.rb
@@ -3,21 +3,21 @@ class Rover
 
   def initialize
     puts "Provide the size of the plateau with two numbers, separated by a space like this: '7 7'"
-    @plateau_size = gets.split(" ")
-    
+    @plateau_size = gets.chomp.split(" ")
+
 
     puts "Provide the coordinates of the rover's location. It must be within the size of the plateau you provided.\n For example, enter '2, 5' with the coordinates separated by a comma."
-    @coordinates = gets
-    @x = @coordinates[0]
-    @y = @coordinates[1]
+    @coordinates = gets.chomp.split(" ")
+    @x = @coordinates[0] # to int?
+    @y = @coordinates[1] # to int?
     puts "The rover's x-coordinate is #{@x} with a y-coordinate of #{@y}."
-    puts "Thanks! Tell me which direction the rover is facing using:\n'n' for North\n's' for South\n'e' for East\n'w' for West.\n"
-    @direction = gets.upcase
+    puts "Thanks! Tell me which direction the rover is facing using: \nn for North \ns for South \ne for East \nw for West."
+    @direction = gets.chomp.upcase
   end
 
   def read_instruction
     puts "Give me a series of instructions on where to move using 'L' for rotate left, 'R' for rotate right, and 'M' to move; you don't need to put spaces between these, so it can look like 'LMRMML'. Cool? direct me!:"
-    @instructions = gets.split("") # => gives you an array of ["L", "M"...]
+    @instructions = gets.chomp.split("") # => gives you an array of ["L", "M"...]
 
 
   end
