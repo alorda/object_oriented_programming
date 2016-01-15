60d3d65d18e935749f553ea5bc2b8453cff54340
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-15 01:01:57 -0500

    rover2.rb has no errors but I can't get it to run past line 63...

--- rover2.rb
+++ rover2.rb
@@ -1,5 +1,5 @@
 module Turn
-  def turn(instructions)
+  def turn(instruction)
       if @direction = "N" && @instructions = "R"
         @direction = "E"
       elsif @direction = "N" && @instructions = "L"
@@ -36,13 +36,19 @@ module Move
       @x_coordinate -= 1
     end
   end
+end
 
 
 class Rover
+
+  include Turn
+  include Move
+
   attr_accessor :x_coordinate, :y_coordinate, :direction, :name
 
 # Changing name of initialize method to read_instruction because we have attr_accessors which help us initialize the rovers.
   def read_instruction(x_coordinate, y_coordinate, direction)
+
     puts "Provide the size of the plateau with two numbers, separated by a space like this: '7 7'"
     @plateau_size = gets.chomp.split(" ")
     puts "     *****\nPlateau width is #{@plateau_size[0]} units by #{@plateau_size[1]} height units."
@@ -55,24 +61,26 @@ class Rover
     @y_coordinate = @coordinates[1] # to int?
     @direction = @coordinates[2].upcase
     puts "     *****\nThe rover is at coordinates #{@x_coordinate}, #{@y_coordinate} facing #{@direction}.\n     *****"
-  # end
-  # I'm commenting out the end above and the 'read_instruction' method below
-  # def read_instruction
+
     puts "Give me a series of instructions on where to move using 'L' for rotate left, 'R' for rotate right, and 'M' to move; you don't need to put spaces between these, so it can look like 'LMRMML'. Cool? direct me!:"
+
     @instructions = gets.chomp.split("") # => gives you an array of ["L", "M"...]
     # We need to take that array of instructions and
     # turn it into something meaningful
     # The robort has to turn first (if it's told to L or R) and then Move after it has turned.
 
+    @instructions.each do |instruction|
+        if instruction == "L" || instruction == "R"
+          turn(instruction)
+        elsif
+          instruction == "M"
+          move
+        end
+
+    end
+  end
 end
 
-
-# Program Begins
-
-# rover1 = Rover.new which will d_instruction
-
-# Decide the new direction
-
-# Based on direction, decide whether you're +/- x or y
-
-# Based on the move, output new coordinates based on original starting point.
+#Program Starts
+rover1 = Rover.new
+rover1 = Rover.name
