d2010d787e958dfeb8d36971d0cb1c02afcbe3ca
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-14 22:47:36 -0500

    Added move method to rover.rb

--- rover.rb
+++ rover.rb
@@ -1,16 +1,18 @@
 class Rover
-# attr_accessor :x, :y, :dir,
+attr_accessor :x_coordinate, :y_coordinate, :heading,
 
   def initialize
     puts "Provide the size of the plateau with two numbers, separated by a space like this: '7 7'"
     @plateau_size = gets.chomp.split(" ")
+    puts "Plateau width is #{@plateau_size[0].to_s} units by #{@plateau_size[1].to_s} height units."
 
     puts "Provide the coordinates of the rover's location. It must be within the size of the plateau you provided.\n For example, enter '2, 5' with the coordinates separated by a comma and space."
     @coordinates = gets.chomp.split(", ")
-    @x = @coordinates[0] # to int?
-    @y = @coordinates[1] # to int?
+    @x_coordinate = @coordinates[0] # to int?
+    @y_coordinate = @coordinates[1] # to int?
     puts "The rover's x-coordinate is #{@x} with a y-coordinate of #{@y}.Thanks!\nTell me which direction the rover is facing using: \nn for North \ns for South \ne for East \nw for West."
-    @direction = gets.chomp.upcase
+    @heading = gets.chomp.upcase
+    puts "The rover is facing #{@heading}."
   end
 
   def read_instruction
@@ -33,6 +35,18 @@ class Rover
 
 
   def move
+    case @heading
+    when "N"
+      y_coordinate =+ 1
+    when "S"
+      y_coordinate -= 1
+    when "E"
+      x_coordinate += 1
+    when "W"
+      x_coordinate -= 1
+    end
+  end
+
 
 
   end
